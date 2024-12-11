c592e938dae7d67cf2cfa61f94f0093eab8fa43a
enyi5938 <Enes.yildiz.2403@gmail.com>
2024-12-11 17:38:20 +0100

    verifyPasswordAndCreateJWT erstellt

--- src/services/JWTService.ts
+++ src/services/JWTService.ts
@@ -1,7 +1,32 @@
-export async function verifyPasswordAndCreateJWT(campusID: string, password: string): Promise<string | undefined> {
-    throw new Error("Function verifyPasswordAndCreateJWT not implemented");
+import { LoginResource } from "../Resources";
+import { JwtPayload, sign, verify } from "jsonwebtoken";
+import { login } from "./AuthenticationService";
+
+export async function verifyPasswordAndCreateJWT(
+  campusID: string,
+  password: string
+): Promise<string | undefined> {
+  const logger = await login(campusID, password);
+  if (!logger) {
+    return undefined;
+  }
+  const payload: JwtPayload = {
+    sub: logger.id,
+    role: logger.role,
+  };
+  const secret = process.env.JWT_SECRET;
+  const ttl = process.env.JWT_TTL
+  if (!secret || !ttl) {
+    throw new Error("JWT_SECRET oder JWT_TTL icht gegeben");
+  }
+  const jwtString = sign(payload, secret, {
+    expiresIn: parseInt(ttl, 10),
+    algorithm: "HS256",
+  });
+
+  return jwtString;
 }
 
 export function verifyJWT(jwtString: string | undefined): LoginResource {
-    throw new Error("Function verifyJWT not implemented");
+  throw new Error("Function verifyJWT not implemented");
 }
